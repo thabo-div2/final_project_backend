7d4f8c2d050cd65c07b1cb3698331cba059d6873
Thabo Setsubi <thabosetsubi3@gmail.com>
2021-08-24 13:38:00 +0200

    Update

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ import sqlite3
 import re
 
 
+# to make the data into a dictionary
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 # Creating Database
 # Initialising admin table
 def init_admin_table():
@@ -41,6 +49,7 @@ def init_patients_table():
     conn.close()
 
 
+# initialising illness table with a foreign table linking to the patients
 def init_illness_table():
     with sqlite3.connect("dentists.db") as conn:
         conn.execute("CREATE TABLE IF NOT EXISTS illness ("
@@ -64,13 +73,70 @@ app.debug = True
 
 @app.route('/', methods=['GET'])
 def welcome():
-    return "Welcome To The Dentist Registry"
+    response = {}
+    if request.method == "GET":
+        response['message'] = "Welcome"
+    return response
+
+
+@app.route('/admin/', methods=["POST"])
+def admin_registration():
+    # route to register admin registration
+    response = {}
+    first_name = request.json["first_name"]
+    last_name = request.json["last_name"]
+    email = request.json["email"]
+    username = request.json["username"]
+    password = request.json["password"]
+    # to check if email is valid
+    ex = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+    if request.method == "POST":
+        if re.search(ex, email):
+            with sqlite3.connect("dentists.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO admin("
+                               "first_name,"
+                               "last_name,"
+                               "email,"
+                               "username,"
+                               "password) VALUES(?, ?, ?, ?, ?)", (first_name, last_name, email, username, password))
+                conn.commit()
+                response['message'] = "admin registered successfully"
+                response['status_code'] = 201
+                response['data'] = {
+                    "first name": first_name,
+                    "last name": last_name,
+                    "email": email,
+                    "username": username,
+                    "password": password
+                }
+            return response
+
+
+@app.route('/login', methods=["PATCH"])
+def login():
+    response = {}
+    # Login using patch method
+    if request.method == "PATCH":
+        username = request.json["username"]
+        password = request.json["password"]
+
+        with sqlite3.connect("dentists.db") as conn:
+            conn.row_factory = dict_factory
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM admin WHERE username=? AND password=?", (username, password))
+            admin = cursor.fetchone()
+
+        response['status_code']  = 200
+        response['data'] = admin
+        return response
 
 
 @app.route('/patient', methods=['POST'])
 def patient_registration():
     global phone_num, id_num
     response = {}
+    # to check if email is valid
     ex = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
     try:
         if request.method == "POST":
@@ -129,7 +195,9 @@ def patient_registration():
 @app.route('/view-patient/<int:patient_id>', methods=["GET"])
 def view_patient(patient_id):
     response = {}
+    # a route to check one patient
     with sqlite3.connect("dentists.db") as conn:
+        conn.row_factory = dict_factory
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM patients WHERE patient_id=" + str(patient_id))
         patient = cursor.fetchone()
@@ -142,13 +210,57 @@ def view_patient(patient_id):
 @app.route('/view-patient/', methods=['GET'])
 def fetch_patients():
     response = {}
+    # a route to check all the patients
     with sqlite3.connect("dentists.db") as conn:
+        conn.row_factory = dict_factory
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM patients")
 
         response['status_code'] = 200
         response['message'] = "Fetched all patients"
         response['data'] = cursor.fetchall()
+    return response
+
+
+@app.route('/illness/<int:patient_id>', methods=['POST'])
+def illness(patient_id):
+    response = {}
+    # a route to register a patients illness
+    name = request.json['name']
+    ill_type = request.json['type']
+    description = request.json['description']
+    patient_id = patient_id
+    if request.method == "POST":
+        with sqlite3.connect("dentists.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO illness("
+                           "name, "
+                           "type, "
+                           "description,"
+                           "patient_id) VALUES(?, ?, ?, ?)", (name, ill_type, description, patient_id))
+            conn.commit()
+            response['message'] = "success"
+            response['status_code'] = 200
+            response['data'] = {
+                "name": name,
+                "type": ill_type,
+                "description": description,
+            }
+        return response
+
+
+@app.route('/view-illness/<int:patient_id>')
+def view_illness(patient_id):
+    response = {}
+    # a route to view a patients illness
+    with sqlite3.connect("dentists.db") as conn:
+        conn.row_factory = dict_factory
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM illness WHERE patient_id = ?", str(patient_id))
+        ill = cursor.fetchone()
+        response['data'] = ill
+        response['message'] = "Success"
+        response['status_code'] = 200
     return response
 
 
